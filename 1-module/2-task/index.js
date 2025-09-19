e3eb2d97720b4cd9a186359de5ce9311960d78d5
plastinin <plastinin_a@it-capital.ru>
2025-09-19 16:05:04 +0300

    #1-2 isValid

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  return !(!name || (name.length < 4) || name.indexOf(' ') > 0);
 }
 
 function sayHello() {
