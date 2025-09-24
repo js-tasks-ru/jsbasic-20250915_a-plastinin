4ff7e8d36e0460f9e06fd3b71f76182e122cbc45
plastinin <plastinin_a@it-capital.ru>
2025-09-24 16:50:37 +0300

    3 module task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
-function camelize(str) {
-  // ваш код...
+function camelize(str) { 
+  return str.split('-')
+          .map(
+            (value, ind) => ind === 0 ? value : value[0].toUpperCase() + value.slice(1)
+          )
+          .join('');
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,20 @@
 function getMinMax(str) {
-  // ваш код...
+  
+  const arrayNumbers = str.split(' ').filter((value) => isFinite(value));
+  
+  switch (arrayNumbers.length) {
+    case 0:
+      arrayNumbers.push(0); // значение по умолчанию
+      break; 
+    case 1: 
+      break;
+    default: 
+      arrayNumbers.sort((a, b) => a - b);
+  }
+
+  return {
+    min: +arrayNumbers[0],      // shift()исключен для обработки ситуации  
+    max: +arrayNumbers.pop(),   // когда в строке одно числовое значение
+  };
+
 }
