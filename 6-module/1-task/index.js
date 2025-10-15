01974dcd5b790887a8ee7fb5437c016f4a1b9587
plastinin <plastinin_a@it-capital.ru>
2025-10-15 19:50:54 +0300

    #6-1 UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,56 @@
  *
  */
 export default class UserTable {
+  #elem;
+
+  get elem() {
+    return this.#elem;
+  }
+
   constructor(rows) {
+    // соберем таблицу
+    this.#elem = document.createElement("table");
+    const theadTag = document.createElement("thead");
+    const tbodyTag = document.createElement("tbody");
+
+    let tableRowHeader = this.#createTableRow(rows[0], true);
+    theadTag.append(tableRowHeader);
+    this.#elem.append(theadTag);
+
+    rows.forEach((element) => {
+      let tableRow = this.#createTableRow(element);
+      tbodyTag.append(tableRow);
+    });
+
+    this.#elem.append(tbodyTag);
+
+    this.#elem.onclick = (event) => {
+      if (event.target.tagName != "BUTTON") return;
+      event.target.closest("tr").remove();
+    };
+  }
+
+  #createTableRow(rowValue, isHeader = false) {
+    const trTag = document.createElement("tr");
+    const tagName = isHeader ? "th" : "td";
+
+    for (let key in rowValue) {
+      const tdTag = document.createElement(tagName);
+      tdTag.append(isHeader ? key : rowValue[key]);
+      trTag.append(tdTag);
+    }
+
+    // последняя колонка таблицы
+    if (isHeader) {
+      const tdTag = document.createElement(tagName);
+      trTag.append(tdTag);
+    } else {
+      const tdTag = document.createElement(tagName);
+      const buttonTag = document.createElement("button");
+      buttonTag.append("X");
+      tdTag.append(buttonTag);
+      trTag.append(tdTag);
+    }
+    return trTag;
   }
 }
